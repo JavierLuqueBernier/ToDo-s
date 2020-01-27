01d7d2c5ae1b8c50ef71f225dea8514424faf64e
JavierLuqueBernier <alagaesia2004@hotmail.com>
2020-01-27 19:11:30 +0100

    Mejora boton encoger menú

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-jQuery('document').ready(function($){
 
 
-var menuBtn = $('.menu-icon');
-    menu = $('header ul');
+var menuBtn = document.querySelector('.menu-icon');
+    menu = document.querySelector('header ul');
+   /*  menu.style.display = "none"; */
 
-menuBtn.click(function() {
+menuBtn.addEventListener('click',  desplegarMenu)
 
-    if( menu.hasClass('show') ) {
+/* function desplegarMenu(event){
 
-        menu.removeClass('show');
-    } else {
-        menu.addClass ('show');
+    if(menu.style.display == 'none'){
+        menu.style.display = 'block';    // Aqui el javascript lo que hace es inyectar estilos en css, lo cual no está mal pero no es lo mejor
+    }else{                               // ya que puede entrar en conflicto con los @media del propio css
+        menu.style.display = 'none'
     }
     
-})
+} */
 
-})
+
+function desplegarMenu(event){
+   if( menu.className == ''){           // Aqui javascript lo que hace es inyectar una clase en el html, lo cual es más acertado.
+        menu.className = "show";
+   }else{
+       menu.className = "";
+   }
+}
+
+
+
+
